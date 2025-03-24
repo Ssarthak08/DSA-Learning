8f77469acd9558aa4d55417c030e9b066700a20a
Sarthak Gulati <gulatisarthak2003@gmail.com>
2025-03-24 23:05:14 +0530

    24-03-2025 first topic

---
+++ DSA/01_list.py
@@ -0,0 +1,13 @@
+# taking input as a list from the user and find its average
+def average(l):
+    sum = 0
+    count = 0
+    for i in l:
+        sum = sum + i
+        count = count + 1
+    total_average = sum / count
+    print(total_average)
+
+
+l = [10, 20, 30, 40]
+print(average(l))
