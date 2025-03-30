f699390a4feddc22407e1fe7e78eb53af86b3537
Sarthak Gulati <gulatisarthak2003@gmail.com>
2025-03-30 21:05:47 +0530

    30-03-2025

---
+++ DSA/07_sorted.py
@@ -0,0 +1,18 @@
+# check if the list is sorted or not
+def sorted():
+    a = []
+    for i in range(5):
+        num = int(input("Enter the numbers : "))
+        a.append(num)
+    print(a)
+
+    for i in range(len(a) - 1):
+        if a[i] < a[i + 1]:
+            print("List is sorted ")
+            break
+        else:
+            print("List is not sorted ")
+
+
+
+sorted()
